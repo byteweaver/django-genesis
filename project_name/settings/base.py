a26d84ed8787395af0a1583bdbb713cc9e4e689f
noxan <noxan@byteweaver.net>
2014-01-14 18:31:30 +0100

    move sqlite database file into deploy folder

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -58,7 +58,7 @@ WSGI_APPLICATION = 'project_name.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'deploy/db.sqlite3'),
     }
 }
 
