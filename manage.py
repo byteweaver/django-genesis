734f4d20f090a2073c13f8f5aba2a7911c8ca25e
noxan <noxan@byteweaver.net>
2014-01-14 18:14:01 +0100

    add settings selection logic to manage.py file

--- manage.py
+++ manage.py
@@ -3,7 +3,13 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_name.settings")
+    target = 'development'
+    if os.path.isfile('.target'):
+        target = open('.target', 'r').readline().strip()
+        if not target or not os.path.isfile('project_name/settings/%s.py' % target):
+            sys.exit("Missing settings file for target '%s'." % target)
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_name.settings.%s" % target)
 
     from django.core.management import execute_from_command_line
 
