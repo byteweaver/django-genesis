cc8edfcbfe2f50603e2173928850e8d8ffcd8b34
noxan <noxan@byteweaver.net>
2014-01-15 18:34:37 +0100

    override database, secrect key, password hasher and email backend in test settings

--- project_name/settings/test.py
+++ project_name/settings/test.py
@@ -1,1 +1,17 @@
 from project_name.settings.base import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+SECRET_KEY = 'UNSECURE_BECAUSE_WE_ARE_TESTING'
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+)
+
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
