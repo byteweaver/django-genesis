6546b6d7aa9f5cdc46307324988e1f5eb0ada04b
noxan <noxan@byteweaver.net>
2014-01-15 18:33:20 +0100

    change project_name placeholder in manage.py file to django variables

--- manage.py
+++ manage.py
@@ -6,10 +6,10 @@ if __name__ == "__main__":
     target = 'development'
     if os.path.isfile('.target'):
         target = open('.target', 'r').readline().strip()
-        if not target or not os.path.isfile('project_name/settings/%s.py' % target):
+        if not target or not os.path.isfile('{{ project_name }}/settings/%s.py' % target):
             sys.exit("Missing settings file for target '%s'." % target)
 
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_name.settings.%s" % target)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings.%s" % target)
 
     from django.core.management import execute_from_command_line
 
