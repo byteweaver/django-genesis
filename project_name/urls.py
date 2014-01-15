89f54b2758804cf68e9685e2f5f544cbcd8163ee
noxan <noxan@byteweaver.net>
2014-01-15 20:00:52 +0100

    add missing newlines to urls.py

--- project_name/urls.py
+++ project_name/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
