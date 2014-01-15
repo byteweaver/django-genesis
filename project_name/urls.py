37e750bf1124051e699bf59abef92ec32723d011
noxan <noxan@byteweaver.net>
2014-01-15 19:58:27 +0100

    add staticfiles_urlpatterns to global urls.py (thanks @akuryou)

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
@@ -10,3 +11,4 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+urlpatterns += staticfiles_urlpatterns()
