58f196e9205b46eee18f789094e0be1712533433
noxan <noxan@byteweaver.net>
2014-04-25 17:11:28 +0200

    add missing newline to urls.py

--- project_name/urls.py
+++ project_name/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', '{{ project_name }}.views.home', name='home'),
