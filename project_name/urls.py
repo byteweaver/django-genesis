ac6ecf09ffe016380b46e7f3f468f9bf2993aefd
noxan <noxan@byteweaver.net>
2014-04-25 14:23:11 +0200

    sort imports in urls.py

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,9 +1,8 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 admin.autodiscover()
